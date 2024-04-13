ab3e9c32f3cc66c00773bc711b921ef2425caae8
Joffre21 <90282920+Joffre21@users.noreply.github.com>
2024-04-12 21:10:45 -0500

    fix/JV: PIZ5-Resolving-fixtures-with-report-tests

--- app/test/fixtures/report.py
+++ app/test/fixtures/report.py
@@ -13,7 +13,31 @@ def month_client_report_mock() -> dict:
         get_random_string: get_random_price(0, 20)
     }
 
-@pytest.fixture()
+@pytest.fixture
+def ingredient_report_uri():
+    return '/report/ingredient'
+
+@pytest.fixture
+def month_report_uri():
+    return '/report/month'
+
+@pytest.fixture
+def client_report_uri():
+    return '/report/client'
+
+@pytest.fixture
+def get_ingredient_report():
+    return ingredient_report_mock()
+
+@pytest.fixture
+def create_month_report():
+    return month_client_report_mock()
+
+@pytest.fixture
+def create_client_report():
+    return month_client_report_mock()
+
+@pytest.fixture
 def create_ingredient_report():
     return ingredient_report_mock()
 
